81a69c0ecb7ff9b00c6b4e0a0552998ab47c3e78
mdyasar49 <mohamedyasar081786@gmail.com>
2026-09-08 10:42:50 +0530

    fix(config): export missing config constants for scraper scripts

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,12 @@ from .settings import settings
 from .schemas import STANDARD_HEADERS, ZOHO_CRM_HEADERS, create_standard_lead
 from .constants import USER_AGENTS, DEFAULT_KEYWORDS, DEFAULT_LOCATIONS
 
+SPREADSHEET_ID_ODOO = settings.SPREADSHEET_ID_ODOO
+SPREADSHEET_ID_ZOHO = settings.SPREADSHEET_ID_ZOHO
+HEADERS = STANDARD_HEADERS
+SERVICE_ACCOUNT_INFO = settings.get_service_account_info()
+CREDENTIALS_FILE = settings.CREDENTIALS_FILE
+
 __all__ = [
     "settings",
     "STANDARD_HEADERS",
@@ -14,4 +20,9 @@ __all__ = [
     "USER_AGENTS",
     "DEFAULT_KEYWORDS",
     "DEFAULT_LOCATIONS",
+    "SPREADSHEET_ID_ODOO",
+    "SPREADSHEET_ID_ZOHO",
+    "HEADERS",
+    "SERVICE_ACCOUNT_INFO",
+    "CREDENTIALS_FILE",
 ]
